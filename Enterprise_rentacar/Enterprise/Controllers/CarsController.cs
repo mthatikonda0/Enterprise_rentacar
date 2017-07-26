1ea168205ef76db144a2f6c38c3a1b0d90a9fce3
mounish thatikonda <mounish.eceb@gmail.com>
2017-07-26 16:08:12 -0400

    Initial layout
    adding pocos

---
+++ Enterprise_rentacar/Enterprise/Controllers/CarsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Enterprise.Models;
+using System.Web.Mvc;
+using Enterprise.ViewModels;
+
+namespace Enterprise.Controllers
+{
+    public class CarsController : Controller
+    {
+
+
+
+        public ViewResult Index()
+        {
+            var cars = GetCars();
+            return View(cars);
+        }
+
+        public IEnumerable<Car> GetCars()
+        {
+            return new List<Car>
+            {
+                new Car {Id = 1, Name = "Civic" },
+                new Car {Id = 2, Name = "Accord" }
+            };
+        }
+        // GET: Cars/random
+        public ActionResult Random()
+        {
+            Car car = new Car() { Name = "civic" };
+
+            var Customers = new List<Customer>()
+            {
+                new Customer {Name = "Customer1" },
+                new Customer {Name = "customer2" }
+            };
+            var viewmodel = new RandomCarViewModel()
+            {
+                Car = car,
+                Customers = Customers
+
+            };
+            return View(viewmodel);
+
+        }
+
+    }
+}
